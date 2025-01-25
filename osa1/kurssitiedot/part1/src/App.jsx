93ee689d63d4268f98a9b3d107def0b89c1ce526
Hanna Varkki <h.varkki@gmail.com>
2025-01-25 17:14:22 +0200

    osa1 tehtävät 1.3-1.5

--- osa1/kurssitiedot/part1/src/App.jsx
+++ osa1/kurssitiedot/part1/src/App.jsx
@@ -3,7 +3,7 @@ const Header = (props) => {
   return (
     <div>
       <h1>
-        {props.course}
+        {props.course.name}
       </h1>
     </div>
   )
@@ -35,42 +35,36 @@ const Total = (props) => {
   return (
     <div>
       <p>
-        Tehtäviä yhteensä {props.total}
+        Tehtäviä yhteensä {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
       </p>
     </div>
   )
 }
 
 const App = () => {
-
-  const course = 'Half Stack application development'
-
-  const parts = [
-
-    {
-      name: 'Fundamentals of React',
-      exercises: 10,
-    },
-
-    {
-      name: 'Using props to pass data',
-      exercises: 7,
-    },
-
-    {
-      name: 'State of a component',
-      exercises: 14,
-    }
-
-  ]
-
-  const totalExercises = parts[0].exercises + parts[1].exercises + parts[2].exercises
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={parts}/>
-      <Total total = {totalExercises}  />
+      <Content parts={course.parts}/>
+      <Total parts = {course.parts}  />
     </div>
   )
 }
