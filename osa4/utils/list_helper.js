0b5c2d53473d24720ebc7cbb09ac11a48f66d2b7
Hanna Varkki <h.varkki@gmail.com>
2025-02-20 18:33:16 +0200

    osa 4 tehtävät 4.3-4.4

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -1,7 +1,15 @@
 const dummy = (blogs) => {
-    // ...
+    return 1
   }
 
-  module.exports = {
-    dummy
+const totalLikes = (blogs) => {
+    const reducer = (sum, blog) => {
+        return sum + blog.likes
+    }
+    return blogs.reduce(reducer, 0)
+}
+
+module.exports = {
+    dummy,
+    totalLikes
   }
