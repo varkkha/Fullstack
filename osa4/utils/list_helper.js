3fbae8d31e004b560b5261a260268b56e7bdcdb6
Hanna Varkki <h.varkki@gmail.com>
2025-02-21 10:00:56 +0200

    osa 4 tehtävät 4.3-4.5

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -9,7 +9,21 @@ const totalLikes = (blogs) => {
     return blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+
+    if (blogs.length === 0) {
+        return null
+      }
+
+    const reducer = (max, item) => {
+        return max.likes > item.likes ? max : item
+    }
+
+    return blogs.reduce(reducer, blogs[0])
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
   }
