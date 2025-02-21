1082266bebc0be6cfabf4495f694fd90f3493f6d
Hanna Varkki <h.varkki@gmail.com>
2025-02-21 15:41:32 +0200

    osa 4 tehtävät 4.11-4.13

--- osa4/tests/blog_api.test.js
+++ osa4/tests/blog_api.test.js
@@ -60,6 +60,55 @@ test('a valid blog can be added ', async () => {
     assert(title.includes('otsikko3'))
   })
 
+  test('if likes are not given, set to 0', async () => {
+    const newBlog = {
+        title: 'otsikko4',
+        author: 'tekijä',
+        url: 'url4'
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/blogs')
+
+    const addedBlog = response.body.find(blog => blog.title === 'otsikko4')
+
+    assert.strictEqual(addedBlog.likes, 0)
+  })
+
+  test('blog without title or url is added', async () => {
+    const newBlog = {
+      author: 'tekijä5'
+    }
+
+    await api.post('/api/blogs').send(newBlog).expect(400)
+
+    const response = await api.get('/api/blogs')
+
+    assert.strictEqual(response.body.length, helper.initialBlogs.length)
+  })
+
+  test('deletion of a blog', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+      const blogToDelete = blogsAtStart[0]
+
+      await api
+        .delete(`/api/blogs/${blogToDelete.id}`)
+        .expect(204)
+
+      const blogsAtEnd = await helper.blogsInDb()
+
+      assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1)
+
+      const titles = blogsAtEnd.map(r => r.title)
+      assert(!titles.includes(blogToDelete.title))
+    })
+
+
 after(async () => {
   await mongoose.connection.close()
 })
