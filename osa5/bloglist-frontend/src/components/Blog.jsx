5975236faf8296802047fa763bf410f7e4a55127
Hanna Varkki <h.varkki@gmail.com>
2025-03-23 17:19:05 +0200

    osa 5 tehtävä 5.7

--- osa5/bloglist-frontend/src/components/Blog.jsx
+++ osa5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,39 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const handleToggleDetails = () => {
+    setShowDetails(!showDetails)
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={handleToggleDetails}>
+          {showDetails ? 'hide' : 'view'}
+        </button>
+      </div>
+
+      {showDetails && (
+        <div>
+          <div>{blog.url}</div>
+          <div>likes {blog.likes}</div>
+          <button>Like</button>
+          <div>{blog.user.username}</div>
+        </div>
+      )}
+    </div>
+  )
+}
 
 export default Blog
